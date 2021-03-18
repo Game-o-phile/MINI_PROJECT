589e3a454bd47c8e3c5cf8f2f37ec99e50bff003
Game-o-phile <69106134+Game-o-phile@users.noreply.github.com>
2021-03-18 21:33:06 +0530

    Update client.py

--- client.py
+++ client.py
@@ -7,6 +7,11 @@ infoObject = pygame.display.Info()
 width = infoObject.current_w
 height = infoObject.current_h
 
+#FONTS
+font1=pygame.font.SysFont('arial',80,bold=True)
+font2=pygame.font.SysFont('arial',18,bold=True)
+font3=pygame.font.SysFont('arial',60,bold=True,italic=True)
+
 #COLOR
 black = (0,0,0)
 white = (255,255,255)
@@ -16,10 +21,6 @@ blue = (0,0,255)
 
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
-
-font1 = pygame.font.SysFont('comicsans', 40)
-
-print(pygame.FULLSCREEN)
 
 class Player:
 
@@ -31,24 +32,29 @@ class Player:
         self.public_key = random.randrange(10, 100000000)
 
 def LandingPage(win, player, image):
-    win.fill((255, 255, 255))
+    win.fill(white)
     # image
     win.blit(image, (0, 0))
 
     # text
-    text = font1.render('Name: ' + str(player.name), 1, (255, 0, 0))  # 1 is for bold or italics (check)
+    text = font3.render('Name: ' + str(player.name), 1, black, white)  # 1 is for bold or italics (check)
     win.blit(text, (10, 10))
-    text = font1.render('1: Transaction ', 1, (255, 0, 0))
-    win.blit(text, (10, 40))
-    text = font1.render('2: Display Blockchain ', 1, (255, 0, 0))
-    win.blit(text, (10, 70))
-    text = font1.render('3: Peer To peer ', 1, (255,0,0))
+    text = font3.render('1: Transaction ', 1, black, white)
     win.blit(text, (10, 100))
-    
+    text = font3.render('2: Display Blockchain ', 1, black, white)
+    win.blit(text, (10, 190))
+    text = font3.render('3: Peer To peer ', 1, black, white)
+    win.blit(text, (10, 280))
+    text = font3.render('4: Blockchain security ', 1, black, white)
+    win.blit(text, (10, 370))
+    text = font3.render('5: Generate sha256 hash', 1, black, white)
+    win.blit(text, (10, 460))
+
     pygame.display.update()
 
+def welcome(win, player, image):
 
-def redrawWindow(win, player, image):
+    win.fill(white)
     win.blit(image, (0,0))
 
     #texts
@@ -56,11 +62,11 @@ def redrawWindow(win, player, image):
 
     # text1 = font3.render('made by:', True, white)
     # text2 = font3.render('Pranav', True, (0, 191, 255))
-    # text3 = font3.render('Aman', True, (0, 191, 255))
-    # text4 = font3.render('Gaurav', True, (0, 191, 255))
+    # text3 = font3.render('Hardik', True, (0, 191, 255))
+    # text4 = font3.render('Gokul', True, (0, 191, 255))
 
     midx = 25
-    midy = height/2 - 100
+    midy = height/2 - 300
 
     win.blit(text, (midx, midy))
     # win.blit(text1, (midx, midy + 100))
@@ -77,7 +83,7 @@ def main(p):
 
     image = pygame.transform.scale(pygame.image.load(r'C:\Users\Isha Pranav\Desktop\miniProj\miniProject\images\bit.jpg'), (width, height))
 
-    redrawWindow(win, p, image)
+    welcome(win, p, image)
 
     while run:
         clock.tick(60)
@@ -95,6 +101,17 @@ def main(p):
                     
                 if event.key == pygame.K_r:
                     LandingPage(win, p, image)
+
+                if event.key == pygame.K_1:
+                    transaction()
+                if event.key == pygame.K_2:
+                    
+                if event.key == pygame.K_3:
+
+                if event.key == pygame.K_4:
+
+                if event.key == pygame.K_5:
+
 
 p=Player('Pranav')
 main(p)
